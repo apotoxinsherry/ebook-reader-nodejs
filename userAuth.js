3e1c4e1e8321e701d2ecdd75d4be699e640e5967
Anirudh Ganapathy <anirudhganapathy2003@gmail.com>
2023-04-11 20:12:58 +0530

    schema for user

--- userAuth.js
+++ userAuth.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+require('dotenv').config();
+
 
 mongoose.connect("mongodb://localhost:27017/users");
 
@@ -6,4 +9,28 @@ const userSchema = new mongoose.Schema({
     userName: String, 
     password: String
 });
+
+
+var secret = process.env.superSecretKey;
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+
+const User = new mongoose.model("user", userSchema);
+
+async function validateUser(userName, password) {
+    const user = await User.findOne({userName: userName});
+    if (user !== null) {
+        if(user.password === password) {
+            return true;
+        }
+        return false;
+    }
+
+    return false;
+}
+
+exports.validateUser = validateUser;
+
+
+
+
 
