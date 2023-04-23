0dcd538d1778215a1f6a1d9b3786dfb909e67ff3
Anirudh Ganapathy <anirudhganapathy2003@gmail.com>
2023-04-23 16:39:58 +0530

    removed unused stuff

--- userModule.js
+++ userModule.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 const passportLocalMongoose = require("passport-local-mongoose");
 require('dotenv').config();
 
@@ -12,24 +11,11 @@ const userSchema = new mongoose.Schema({
 });
 
 
-var secret = process.env.superSecretKey;
-//userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("user", userSchema);
 
-// async function validateUser(userName, password) {
-//     const user = await User.findOne({userName: userName});
-//     if (user !== null) {
-//         if(user.password === password) {
-//             return true;
-//         }
-//         return false;
-//     }
-
-//     return false;
-// }
 
 module.exports = User;
 
